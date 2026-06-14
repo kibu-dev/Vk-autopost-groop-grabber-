c310c4d9c059bb9c1bf18674f17eef1c327b835f
kibu-dev <burmakin.kirill@gmail.com>
2026-06-15 02:39:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-import os
 import threading
-from db import init_db, migrate_forbidden_words_from_json
-from config import USER_TOKEN, GROUP_TOKEN, GROUP_ID
+from config import *
 from grabber import run_grabber
 from publisher import run_publisher
 from messenger import run_messenger
 
 if __name__ == "__main__":
     if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
-        print("❌ Ошибка: проверьте USER_TOKEN, GROUP_TOKEN, GROUP_ID в .env")
+        print("❌ Проверьте .env")
         exit(1)
-    
-    init_db()
-    print("✅ База данных готова")
-    
-    if os.path.exists("forbidden_words.json"):
-        migrate_forbidden_words_from_json()
-        os.rename("forbidden_words.json", "forbidden_words.json.bak")
-        print("✅ JSON со словами перенесён в БД")
     
     threading.Thread(target=run_grabber, daemon=True).start()
     threading.Thread(target=run_publisher, daemon=True).start()
