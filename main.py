d15f929a93f9b1927fcb7603a407de47ff80e784
kibu-dev <burmakin.kirill@gmail.com>
2026-06-15 02:57:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ from messenger import run_messenger
 
 if __name__ == "__main__":
     if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
-        print("❌ Проверьте .env")
-        exit(1)
-    
+        print("❌ Проверьте .env"); exit(1)
     threading.Thread(target=run_grabber, daemon=True).start()
     threading.Thread(target=run_publisher, daemon=True).start()
-    
     print("🚀 Бот запущен!")
     run_messenger()
