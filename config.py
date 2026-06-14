00f9d686991c8fbe46250a9b6836d24de7455e31
kibu-dev <burmakin.kirill@gmail.com>
2026-06-15 02:36:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Основные токены
 USER_TOKEN = os.getenv("USER_TOKEN")
 GROUP_TOKEN = os.getenv("GROUP_TOKEN")
 GROUP_ID = int(os.getenv("GROUP_ID"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
-# Интервалы (в секундах)
 CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "60"))
 PUBLISH_INTERVAL = int(os.getenv("PUBLISH_INTERVAL", "900"))
 GRAB_INTERVAL = int(os.getenv("GRAB_INTERVAL", "1800"))
 
-# Граббер
 GRAB_POSTS_PER_GROUP = int(os.getenv("GRAB_POSTS_PER_GROUP", "3"))
 MAX_GRAB_PER_GROUP_DAY = int(os.getenv("MAX_GRAB_PER_GROUP_DAY", "2"))
 
-# Настройки поста от граббера
 GRABBER_POST_PREFIX = os.getenv("GRABBER_POST_PREFIX", "")
 GRABBER_POST_SUFFIX = os.getenv("GRABBER_POST_SUFFIX", "")
