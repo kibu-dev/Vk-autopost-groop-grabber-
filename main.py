37ef9743a6820b7fb9ca48b33a9e96de1a659332
kibu-dev <burmakin.kirill@gmail.com>
2026-06-16 12:52:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import threading
 from config import *
 from grabber import run_grabber
-from pub_grabber import run_pub_grabber
 from pub_users import run_pub_users
 from messenger import run_messenger
 
 if __name__ == "__main__":
     if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
         print("❌ Проверьте .env"); exit(1)
-    
+
     threading.Thread(target=run_grabber, daemon=True).start()
-    threading.Thread(target=run_pub_grabber, daemon=True).start()
     threading.Thread(target=run_pub_users, daemon=True).start()
-    
+
     print("🚀 Бот запущен!")
     run_messenger()
