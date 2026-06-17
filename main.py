f7a0bc8d5058a4a9a858b022dc25c1508279c1b5
kibu-dev <burmakin.kirill@gmail.com>
2026-06-17 14:33:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from config import *
 from grabber import run_grabber
 from pub_users import run_pub_users
 from messenger import run_messenger
+from auto_liker import run_auto_liker
+from online_keeper import run_online_keeper
+from friend_acceptor import run_friend_acceptor
 
 if __name__ == "__main__":
     if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
@@ -10,6 +13,9 @@ if __name__ == "__main__":
 
     threading.Thread(target=run_grabber, daemon=True).start()
     threading.Thread(target=run_pub_users, daemon=True).start()
+    threading.Thread(target=run_auto_liker, daemon=True).start()
+    threading.Thread(target=run_online_keeper, daemon=True).start()
+    threading.Thread(target=run_friend_acceptor, daemon=True).start()
 
     print("🚀 Бот запущен!")
     run_messenger()
