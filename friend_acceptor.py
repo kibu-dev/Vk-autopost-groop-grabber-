b715e176a297a7379108fdff44535b8d54d759d8
kibu-dev <burmakin.kirill@gmail.com>
2026-06-17 14:37:05 +0800

    Create friend_acceptor.py

---
+++ friend_acceptor.py
@@ -0,0 +1,25 @@
+import time
+import vk_api
+from config import *
+from utils import *
+
+def run_friend_acceptor():
+    vk = vk_api.VkApi(token=USER_TOKEN).get_api()
+    print("🤝 Друзья запущены")
+
+    while True:
+        try:
+            if is_friend_enabled():
+                requests = vk.friends.getRequests(out=0, count=50)
+                if requests["items"]:
+                    for user_id in requests["items"]:
+                        vk.friends.add(user_id=user_id)
+                        add_friend_stat()
+                        print(f"🤝 Принят: {user_id}")
+                        time.sleep(1)
+                time.sleep(900)
+            else:
+                time.sleep(10)
+        except Exception as e:
+            print(f"🤝 Ошибка: {e}")
+            time.sleep(60)
