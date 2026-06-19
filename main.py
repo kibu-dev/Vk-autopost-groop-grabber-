2c2ed6db0f38e35884fba2643a96f92caf581e79
kibu-dev <burmakin.kirill@gmail.com>
2026-06-19 20:01:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from messenger import run_messenger
 from auto_liker import run_auto_liker
 from online_keeper import run_online_keeper
 from friend_acceptor import run_friend_acceptor
+from group_acceptor import run_group_acceptor
 
 if __name__ == "__main__":
     if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     threading.Thread(target=run_auto_liker, daemon=True).start()
     threading.Thread(target=run_online_keeper, daemon=True).start()
     threading.Thread(target=run_friend_acceptor, daemon=True).start()
+    threading.Thread(target=run_group_acceptor, daemon=True).start()
 
     print("🚀 Бот запущен!")
     run_messenger()
