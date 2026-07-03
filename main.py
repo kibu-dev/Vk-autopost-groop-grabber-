019f23df0cc4fe75fd7ba09eac7108c359418d0a
kibu-dev <burmakin.kirill@gmail.com>
2026-07-04 00:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from online_keeper import run_online_keeper
 from friend_acceptor import run_friend_acceptor
 from group_acceptor import run_group_acceptor
 from weekly_horoscope import run_weekly_horoscope
+from tg_grabber import run_tg_grabber
 
-# Настройка логирования
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -38,5 +38,6 @@ if __name__ == "__main__":
     threading.Thread(target=run_friend_acceptor, daemon=True).start()
     threading.Thread(target=run_group_acceptor, daemon=True).start()
     threading.Thread(target=run_weekly_horoscope, daemon=True).start()
+    threading.Thread(target=run_tg_grabber, daemon=True).start()
 
     run_messenger()
