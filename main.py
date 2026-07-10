3b807175651d31f1fe9da09d4c1d951e1da30f31
kibu-dev <burmakin.kirill@gmail.com>
2026-07-10 08:45:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ import sys
 from logging.handlers import RotatingFileHandler
 from config import *
 from grabber import run_grabber
-from pub_users import run_pub_users
 from messenger import run_messenger
-from auto_liker import run_auto_liker
-from online_keeper import run_online_keeper
-from friend_acceptor import run_friend_acceptor
 from group_acceptor import run_group_acceptor
 from weekly_horoscope import run_weekly_horoscope
 
@@ -31,15 +27,11 @@ logger.addHandler(file_handler)
 logging.info("🚀 Бот запускается...")
 
 if __name__ == "__main__":
-    if not USER_TOKEN or not GROUP_TOKEN or not GROUP_ID:
-        logging.error("❌ Проверьте .env")
+    if not GROUP_TOKEN or not GROUP_ID:
+        logging.error("❌ Проверьте .env (GROUP_TOKEN, GROUP_ID)")
         exit(1)
 
     threading.Thread(target=run_grabber, daemon=True).start()
-    threading.Thread(target=run_pub_users, daemon=True).start()
-    threading.Thread(target=run_auto_liker, daemon=True).start()
-    threading.Thread(target=run_online_keeper, daemon=True).start()
-    threading.Thread(target=run_friend_acceptor, daemon=True).start()
     threading.Thread(target=run_group_acceptor, daemon=True).start()
     threading.Thread(target=run_weekly_horoscope, daemon=True).start()
 
